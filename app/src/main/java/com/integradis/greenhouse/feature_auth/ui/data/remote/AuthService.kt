423cbcd41b7419783f8bb23c0b8fb41a1543fc73
Eric Cuevas <99770864+Ericcuevas03@users.noreply.github.com>
2024-05-24 00:05:36 -0500

    Update AuthService.kt

--- app/src/main/java/com/integradis/greenhouse/feature_auth/ui/data/remote/AuthService.kt
+++ app/src/main/java/com/integradis/greenhouse/feature_auth/ui/data/remote/AuthService.kt
@@ -2,7 +2,9 @@ package com.integradis.greenhouse.feature_auth.ui.data.remote
 
 import retrofit2.Call
 import retrofit2.http.Body
+import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.Query
 
 //usar comando json-server --watch db.json --host 0.0.0.0 --port 3001 , para encender el json server
 
